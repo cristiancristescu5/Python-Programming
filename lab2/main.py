5e840c8c34f636cda566c671f346fe341a44bc5e
Cristian Cristescu <cristian.cristescu27@gmail.com>
2023-10-09 00:26:43 +0300

    bau

--- lab2/main.py
+++ lab2/main.py
@@ -1,4 +1,8 @@
-def fibo(n):
+import math
+import string
+
+
+def fibo(n):  # 1
     fibo_list = []
     if n <= 0:
         print("Incorrect input")
@@ -21,4 +25,151 @@ def fibo(n):
     return fibo_list
 
 
+def is_prime(x):
+    if x == 1:
+        return False
+    if x == 2 or x == 3 or x == 7:
+        return True
+    for d in range(2, int(math.sqrt(x)) + 1):
+        if x % d == 0:
+            return False
+
+    return True
+
+
+def prime_list(x):  # 2
+    result_list = []
+    for i in x:
+        if is_prime(i):
+            result_list.append(i)
+            print(i)
+    return result_list
+
+
+def process_lists(a=[], b=[]):  # 3
+    intersected = []
+    union = []
+    dif1 = []
+    dif2 = []
+    for i in a:
+        if b.count(i) != 0:
+            intersected.append(i)
+    for i in a:
+        if union.count(i) == 0:
+            union.append(i)
+    for i in b:
+        if union.count(i) == 0:
+            union.append(i)
+
+    for i in a:
+        if b.count(i) == 0:
+            dif1.append(i)
+
+    for i in b:
+        if a.count(i) == 0:
+            dif2.append(i)
+
+    return [intersected, union, dif1, dif2]
+
+
+def create_song(i, notes=[], moves=[]):  # 4
+    song = [notes[i]]
+    position = i
+    for j in moves:
+        position += j
+        while position > len(notes) - 1:
+            position = position - len(notes)
+        song.append(notes[position])
+    return song
+
+
+def replace_diagonal(matrix):  # 5
+    for i in range(0, len(matrix[0])):
+        for j in range(0, len(matrix[0])):
+            if i > j:
+                matrix[i][j] = 0
+    return matrix
+
+
+def process_lists_var(*lists, x):
+    result_list = []
+    el = []
+    l: list
+    for l in lists:
+        result_list = list(set(result_list) | set(l))
+    for i in result_list:
+        count = 0
+        for l in lists:
+            count += l.count(i)
+        if count == x:
+            el.append(i)
+    return el
+
+
+def isPalindrome(x):
+    inv = 0
+    y = x
+    while x != 0:
+        inv = inv * 10 + x % 10
+        x = x // 10
+    if inv == y:
+        return True
+    return False
+
+
+def palindrome_tuple(x: list):
+    max_ = 0
+    count = 0
+    for i in x:
+        if isPalindrome(i):
+            count += 1
+            if i > max_:
+                max_ = i
+    return count, max_
+
+
+def ascii_list(string_list: list[string], flag, x=1):
+    result_chars = []
+    for s in string_list:
+        chars = []
+        for c in s:
+            if flag and ord(c) % x == 0:
+                chars.append(c)
+            if not flag and ord(c) % x != 0:
+                chars.append(c)
+        result_chars.append(chars)
+    return result_chars
+
+
+def spectators(seats: list[list]):
+    rows = len(seats) - 1
+    cols = len(seats[0])
+    pos = []
+    for j in range(cols):
+        # index = rows
+        for i in range(rows, 0, -1):
+            el = seats[i][j]
+            print(el, i, j)
+            for m in range(i-1, 1, -1):
+                if seats[m][j] > el or seats[m][j] == el:
+                    if pos.count((m, j, seats[m][j])) == 0:
+                        pos.append((m, j, seats[m][j]))
+    # for i in range(rows):
+    #     for j in range(cols):
+    #         el = seats[i][j]
+    #         for m in range(i + 1, rows):
+    #             if seats[m][j] >= el and pos.count((i, j)) == 0:
+    #                 pos.append((i, j))
+    #                 break
+    return pos
+
+
 print(fibo(3))
+print(prime_list([1, 2, 3, 4, 5, 6, 7, 12, 17, 11, 13, 14, 25, 23, 78, 31, 41, 51]))
+print(process_lists([1, 2, 3], [1, 3, 4, 5]))
+print(create_song(2, ["do", "re", "mi", "fa", "sol"], [1, -3, 4, 2]))
+print(replace_diagonal([[1, 2, 3], [1, 2, 3], [1, 2, 3]]))
+print(process_lists_var([1, 2, 3], [2, 3, 4], [4, 5, 6], [4, 1, "test"], x=1))
+print(palindrome_tuple([121, 131, 141, 151, 161, 171, 100001]))
+print(ascii_list(["test", "hello", "lab002"], False, 2))
+print(spectators([[1, 2, 3, 2, 1, 1], [2, 4, 4, 3, 7, 2], [5, 5, 2, 5, 6, 4], [6, 6, 7, 6, 7, 5]]))
